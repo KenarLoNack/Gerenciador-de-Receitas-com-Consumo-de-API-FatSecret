71132681e6b8f8b826bced7e55ad8344deca78e5
Raphael Claudino <raphael.escola.rc@gmail.com>
2025-05-18 20:50:38 -0300

    continuando front-end add recipe, falta terminar colocando o campo de ingredientes (criar uma tela propria para adicionar os ingredientes e aparecer em uma lista na tela de add receita)

--- lib/addRecipe.dart
+++ lib/addRecipe.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/cupertino.dart';
 
 class Addrecipe extends StatelessWidget {
   const Addrecipe({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+
     final TextEditingController nomeController = TextEditingController();
     final TextEditingController tempoController = TextEditingController();
     final TextEditingController porcoesController = TextEditingController();
     final TextEditingController preparoController = TextEditingController();
+
+    tempoController.text = "00:00";
+    porcoesController.text = "1"; // valor inicial para porções
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Adicione uma receita"),
@@ -20,71 +27,236 @@ class Addrecipe extends StatelessWidget {
           icon: Icon(Icons.arrow_back),
         ),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(top: 25, left: 25, bottom: 25),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.zero,
               child: Image.network(
-                'https://placehold.co/80x80?text=Hello+World',
-                width: 150,
-                height: 150,
+                'https://placehold.co/600x600/png',
+                height: 180,
+                width: double.infinity,
                 fit: BoxFit.cover,
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: Wrap(
-              runSpacing: 10,
-              children: [
-                TextField(
-                  controller: nomeController,
-                  decoration: InputDecoration(
-                    labelText: 'Nome da Receita',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                TextField(
-                  controller: tempoController,
-                  decoration: InputDecoration(
-                    labelText: "Tempo de Sono",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                TextField(
-                  controller: porcoesController,
-                  decoration: InputDecoration(
-                    labelText: "Porções",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                TextFormField(
-                  controller: preparoController,
-                  maxLines: 5,
-                  inputFormatters: [
-                    TextInputFormatter.withFunction((oldValue, newValue) {
-                      final newText = newValue.text.replaceAllMapped(
-                        RegExp(r'(^|\n)(?!• )'),
-                        (match) => '${match.group(1)}• ',
-                      );
-                      return TextEditingValue(
-                        text: newText,
-                        selection:
-                            TextSelection.collapsed(offset: newText.length),
-                      );
-                    }),
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: nomeController,
+                      decoration: InputDecoration(
+                        labelText: 'Nome da Receita',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Informe o nome da receita';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 12),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            controller: tempoController,
+                            readOnly: true,
+                            decoration: InputDecoration(
+                              labelText: "Tempo de Preparo",
+                              border: OutlineInputBorder(),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Informe o tempo';
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(4),
+                            ),
+                            padding: EdgeInsets.all(
+                                20), // controle o tamanho interno do botão
+                            backgroundColor:
+                                Colors.grey, // cor de fundo do botão (opcional)
+                          ),
+                          onPressed: () async {
+                            Duration? duration =
+                                await showModalBottomSheet<Duration>(
+                              context: context,
+                              builder: (BuildContext context) {
+                                Duration selectedDuration =
+                                    Duration(minutes: 30);
+                                return SizedBox(
+                                  height: 250,
+                                  child: Column(
+                                    children: [
+                                      Expanded(
+                                        child: CupertinoTimerPicker(
+                                          mode: CupertinoTimerPickerMode.hm,
+                                          initialTimerDuration:
+                                              selectedDuration,
+                                          onTimerDurationChanged:
+                                              (Duration newDuration) {
+                                            selectedDuration = newDuration;
+                                          },
+                                        ),
+                                      ),
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          Navigator.pop(
+                                              context, selectedDuration);
+                                        },
+                                        child: Text("Confirmar"),
+                                      )
+                                    ],
+                                  ),
+                                );
+                              },
+                            );
+
+                            if (duration != null) {
+                              String twoDigits(int n) =>
+                                  n.toString().padLeft(2, '0');
+                              String formatted =
+                                  "${twoDigits(duration.inHours)}:${twoDigits(duration.inMinutes.remainder(60))}";
+                              tempoController.text = formatted;
+                            }
+                          },
+                          child: Icon(
+                            Icons.timer_outlined,
+                            size: 25,
+                            color: Colors.white, // cor do ícone
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 12),
+                    TextFormField(
+                      controller: porcoesController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: "Porções",
+                        border: OutlineInputBorder(),
+                        suffixIcon: IconButton(
+                          icon: Icon(Icons.arrow_drop_down),
+                          onPressed: () {
+                            int initialPorcoes =
+                                int.tryParse(porcoesController.text) ?? 1;
+
+                            showCupertinoModalPopup(
+                              context: context,
+                              builder: (_) => Container(
+                                height: 250,
+                                color: CupertinoColors.systemBackground
+                                    .resolveFrom(context),
+                                child: Column(
+                                  children: [
+                                    Expanded(
+                                      child: CupertinoPicker(
+                                        itemExtent: 32,
+                                        scrollController:
+                                            FixedExtentScrollController(
+                                                initialItem:
+                                                    initialPorcoes - 1),
+                                        onSelectedItemChanged: (int value) {
+                                          porcoesController.text =
+                                              (value + 1).toString();
+                                        },
+                                        children: List.generate(
+                                            100,
+                                            (index) => Center(
+                                                  child: Text('${index + 1}'),
+                                                )),
+                                      ),
+                                    ),
+                                    CupertinoButton(
+                                      child: Text('Confirmar'),
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                    )
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Informe as porções';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 12),
+                    //lista de ingredientes
+
+                    SizedBox(height: 12),
+                    TextFormField(
+                      controller: preparoController,
+                      maxLines: 5,
+                      inputFormatters: [
+                        TextInputFormatter.withFunction((oldValue, newValue) {
+                          if (newValue.text.isEmpty) {
+                            return TextEditingValue(text: '');
+                          }
+
+                          final newText = newValue.text.replaceAllMapped(
+                            RegExp(r'(^|\n)(?!• )'),
+                            (match) => '${match.group(1)}• ',
+                          );
+
+                          final cleanedText =
+                              newText.replaceAll(RegExp(r'•\s*$'), '');
+
+                          return TextEditingValue(
+                            text: cleanedText,
+                            selection: TextSelection.collapsed(
+                                offset: cleanedText.length),
+                          );
+                        }),
+                      ],
+                      decoration: InputDecoration(
+                        labelText: 'Modo de preparo',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Descreva o modo de preparo';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Receita cadastrada com sucesso!'),
+                            ),
+                          );
+                          // Aqui você pode salvar os dados, enviar para o banco, etc.
+                        }
+                      },
+                      child: Text("Cadastrar Receita"),
+                    ),
                   ],
-                  decoration: InputDecoration(
-                    labelText: 'Modo de preparo',
-                    border: OutlineInputBorder(),
-                  ),
                 ),
-              ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'addRecipe.dart';
+import 'addrecipe.dart';
 
 void main() => runApp(MyApp());
 
